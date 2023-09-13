25af668d9e8a9ee538efa46f5b26e4365fd3a836
Muhammad Hamza <143834218+hhamza56@users.noreply.github.com>
2023-09-13 22:53:29 +0100

    work complete

---
+++ apps.js
@@ -0,0 +1,340 @@
+
+
+// question no 1//
+
+// var cityname = prompt("Your City Name")
+
+// if (cityname=== "karachi")
+//  {alert("Welcome To Karachi")
+//  console.log("welcome to karachi");
+
+// var gender = prompt("Your Gender") 
+
+// if (gender=== "male"){alert("welcome to sir ")
+// console.log("welcome to sir ")
+// {
+
+// var trafic = prompt("Your Signal Light")
+
+// if (trafic=== "green")
+// {alert("Line Clear Sir ")
+// console.log("clear sir ")
+// }else alert("sorry")
+
+
+// } 
+// }else alert("sorry")
+
+
+// }else alert("sorry")
+
+
+
+
+
+
+// question 2//
+
+// var cityname = prompt("your city")
+// var gender = prompt(" gender")
+// var trafic = prompt("your signal light")
+
+// if (cityname==="karachi"  &&  gender==="male" && trafic==="green")
+// console.log("welcome to sir ")
+// else{
+// alert("sorry sir")
+// }
+
+
+
+// question 3//
+
+
+
+// var fuel = +prompt (0.25);
+// if (fuel===150){
+//     alert("Car Tank Full");
+
+// }else alert("No Full Tank");
+
+
+
+// question 4//
+
+// var a=7;
+// if (++a===8){console.log("given condition four variabale a is ture:");
+
+// }
+
+//  var b = +prompt ("value ");
+// if  (b++ === 83)
+
+// {
+// alert("given condition for variable b is true");
+// }
+//  var c = +prompt("value");
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// e. if (true) 
+// alert("True");{
+
+// }
+
+// if (false){
+// alert("False");
+// }
+
+
+
+
+
+// question 5//
+
+
+// var firstsubject = +prompt("Enter Your first Subject Marks");
+// var secondSubject = +prompt("Enter Your second Subject Marks");
+// var thirdsubject = +prompt("Enter Your third Subject Marks");
+// var  totalmarks = +prompt("Enter Your Total Marks");
+// var obtmarks = firstsubject + secondSubject + thirdsubject;
+// var percentage = (obtmarks / totalmarks) * 100;
+// if (percentage >= 80 && percentage <= 100){
+//     document.write("percentage" +percentage +"<br>" + "grade" + "A+");
+// }else if (percentage >=70 && percentage <= 80){
+//     document.write ("percentage" + percentage + "<br>" + "grage" + "A");
+// }else if (percentage >= 60 && percentage <=70){
+//     document.write("percentage" +percentage + "<br>" + "grade" +"B")
+// }else if ( percentage >= 20 && percentage <= 30){
+//     document.write(" percentage"+ percentage + "<br>" + "grade" + " fail")
+// }
+
+
+
+
+
+
+// question 6//
+
+// var num1 = +prompt ("SANI NUMBER KA INTYKHAB KARAY SHUKRIYA 1 2 3  487 79  54 44");
+// if (num1 === 1){
+//     alert("WELCOME SIR THANKS ")
+// var num2 = +prompt("DOUSARY NUMBER KA INTUKHAB KARAY SHUKRIYA 1+2+3+5+20")
+// if(num2 === 2) {
+//     alert("WELCOME SIR THANKS ")
+
+// } else alert("SORRY SIR TRY AGAIN")
+// } else alert("SORRY SIR TRY AGAIN")
+
+
+
+
+// question 7//
+
+// var nu = +prompt("Enter Your Number")
+// if ( nu %  3==0){
+//     console.log("yes afcours")
+//     alert("yes afcours")
+// }else alert("no sorry")
+
+
+
+// question 8//
+
+// var Name = prompt("Enter your Name")
+// // var Name2 = prompt("enter Your Last Name")
+// console.log ( "welcome to " + Name );
+
+
+
+
+
+// question 9 //
+
+// var tmp = +prompt("First Day Temperature");
+// if(tmp >=40){
+//     alert("GHAR SY BAHIR MAT JOU")
+//     var tmp1 = +prompt("Second Day temperature")
+//     if(tmp1 <=30){
+//         alert("aj mosam normal hai ")
+//         var tmp2 = +prompt("Third Day temperature")
+//         if(tmp2 <=15){
+//             alert("Aj bht thand hai ")
+//         }else alert("sorry rong")
+        
+//     }else alert("sorry rong")
+// }else alert("sorry rong")
+
+
+
+
+
+
+
+
+
+// question 10//
+
+// // Input the first number, second number, and operator from the user
+// const num1 = parseFloat(prompt("Enter the first number:"));
+// const num2 = parseFloat(prompt("Enter the second number:"));
+// const operator = prompt("Enter the operation (+, -, *, /, %):");
+
+// // Initialize a variable to store the result
+// let result;
+
+// // Check the operator and perform the corresponding calculation
+// if (operator === "+") {
+//     result = num1 + num2;
+// } else if (operator === "-") {
+//     result = num1 - num2;
+// } else if (operator === "*") {
+//     result = num1 * num2;
+// } else if (operator === "/") {
+//     if (num2 === 0) {
+//         alert("Division by zero is not allowed.");
+//     } else {
+//         result = num1 / num2;
+//     }
+// } else if (operator === "%") {
+//     if (num2 === 0) {
+//         alert("Modulus by zero is not allowed.");
+//     } else {
+//         result = num1 % num2;
+//     }
+// } else {
+//     alert("Invalid operator entered.");
+// }
+
+// // Display the result to the user
+// if (result !== undefined) {
+//     alert(`Result: ${num1} ${operator} ${num2} = ${result}`);
+// }
+
+
+
+
+
+
+
+// question 11//
+
+// var ham = prompt("Chose Your Value           Ascll,z=90,a=856,C5851, ");
+// if ( ham ==="C5851"){
+//  alert("good job sir");
+//   } else alert("no try plz")
+ 
+
+
+// question 12//
+
+// var jav = (  "javascript" );
+// var jav1 = ("javascript")
+// alert(jav + "    " + jav1) 
+
+
+
+
+// question 13//
+
+// var nm5 = +prompt("positive, negative or zero.");
+// if (nm5 === 0){
+//     alert(" good sir")
+// } else alert("try again")
+
+
+
+
+// question 14//
+
+// var mh5 = prompt("javascript")
+// if (mh5 === "hello sir welcome"){
+//     alert ("welcomr sir")
+// }else(
+//     alert("sorry sir")
+// )
+
+
+
+
+// question 15//
+
+// var hn = prompt("Enter Your Pssword")
+// if (hn === "passward"){
+//     alert("i well chek sir wat..")
+//     alert("welcome sir ")
+// }else (alert("plx try again"))
+
+
+
+
+// question 16//
+
+// var hours = +prompt("hours");
+// if(hours <18){
+//     alert("good morning")
+// }else (alert("good evening"))
+
+
+// question 17 //
+
+var watch = +prompt("Time")
+if(watch===1900){
+    alert("24 hours")
+    var watch1 = +prompt("time")
+    if (watch1 ===1800){
+        alert("23 hours")
+        var watch2 = +prompt("time")
+        if(watch2===1700){
+            alert("22 hours")
+        }else alert(" try again...")
+    }else alert("try again...")
+} else alert("try again...")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
